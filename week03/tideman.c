e40351f4295aaad8d9cc617930ba1a5f9a972886
w-modz <46196436+w-modz@users.noreply.github.com>
2021-07-20 22:28:50 +0200

    Remove commented out code

--- week03/tideman.c
+++ week03/tideman.c
@@ -177,10 +177,6 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    // for (int i = 0; i < pair_count; i++)
-    // {
-    //     locked[pairs[i].winner][pairs[i].loser] = false;
-    // }
     for (int i = 0; i < pair_count; i++)
     {
         if (!is_circle(pairs[i].winner, pairs[i].loser))
