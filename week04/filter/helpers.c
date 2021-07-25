55d38bc508b5147d92246d0d2a24891d1cb914c5
w-modz <46196436+w-modz@users.noreply.github.com>
2021-07-25 19:48:50 +0200

    Implement grayscale functionality, add clean to makefile

--- week04/filter/helpers.c
+++ week04/filter/helpers.c
@@ -3,6 +3,19 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            uint8_t blue = image[i][j].rgbtBlue;
+            uint8_t green = image[i][j].rgbtGreen;
+            uint8_t red = image[i][j].rgbtRed;
+            uint8_t average = (blue + green + red) / 3;
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+        }
+    }
     return;
 }
 
