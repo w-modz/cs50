f440495aa9d831a97eb639a0a6f78f6f88df6df7
w-modz <46196436+w-modz@users.noreply.github.com>
2021-07-27 18:32:46 +0200

    Implement `edges()`

--- week04/filter-more/helpers.c
+++ week04/filter-more/helpers.c
@@ -1,3 +1,8 @@
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 #include "helpers.h"
 
 // Convert image to grayscale
@@ -18,8 +23,183 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+typedef struct
+{
+    float red;
+    float green;
+    float blue;
+}
+vertical;
+typedef struct
+{
+    float red;
+    float green;
+    float blue;
+}
+horizontal;
+
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Make a copy of the original image
+    RGBTRIPLE(*og_image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    if (og_image == NULL)
+    {
+        fprintf(stderr, "Not enough memory to store image copy.\n");
+        return;
+    }
+    memcpy(og_image, image, height * width * sizeof(RGBTRIPLE));
+
+    RGBTRIPLE adj[9];
+    for(int32_t h = 0; h < height; h++)
+    {
+        for(int32_t w = 0; w < width; w++)
+        {
+            int32_t min_height = h - 1;
+            int32_t max_height = h + 1;
+            int32_t min_width = w - 1;
+            int32_t max_width = w + 1;
+
+            vertical Gx;
+            horizontal Gy;
+            Gx.red = 0;
+            Gx.green = 0;
+            Gx.blue = 0;
+            Gy.red = 0;
+            Gy.green = 0;
+            Gy.blue = 0;
+
+            // Traverse the 3x3 grid of adjacent pixels and calculate Gx and Gy
+            int32_t adjIndex = 0;
+            for (int32_t h2 = min_height; h2 <= max_height; h2++)
+            {
+                for (int32_t w2 = min_width; w2 <= max_width; w2++)
+                {
+                    // Set the pixel color to solid black if outside the image bounds
+                    if (h2 < 0 || w2 < 0 || h2 > height - 1 || w2 > width - 1)
+                    {
+                        adj[adjIndex].rgbtBlue = 0;
+                        adj[adjIndex].rgbtGreen = 0;
+                        adj[adjIndex].rgbtRed = 0;
+                    }
+                    else
+                    {
+                        adj[adjIndex] = og_image[h2][w2];
+                    }
+
+                    if(adjIndex == 0)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * -1;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * -1;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * -1;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * -1;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * -1;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * -1;
+                    }
+                    else if(adjIndex == 1)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * -2;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * -2;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * -2;
+                    }
+                    else if(adjIndex == 2)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 1;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 1;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 1;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * -1;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * -1;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * -1;
+                    }
+                    else if(adjIndex == 3)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * -2;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * -2;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * -2;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                    } 
+                    else if(adjIndex == 4)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                    }
+                    else if(adjIndex == 5)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 2;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 2;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 2;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                    }
+                    else if(adjIndex == 6)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * -1;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * -1;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * -1;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 1;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 1;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 1;
+                    }
+                    else if(adjIndex == 7)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 0;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 0;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 0;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 2;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 2;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 2;
+                    }
+                    else if(adjIndex == 8)
+                    {
+                        Gx.red += (int32_t) adj[adjIndex].rgbtRed * 1;
+                        Gx.green += (int32_t) adj[adjIndex].rgbtGreen * 1;
+                        Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * 1;
+                        Gy.red += (int32_t) adj[adjIndex].rgbtRed * 1;
+                        Gy.green += (int32_t) adj[adjIndex].rgbtGreen * 1;
+                        Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * 1;
+                    }
+                    adjIndex++;
+                    // Calculate the final color values for the pixel
+                    int32_t Red;
+                    int32_t Green;
+                    int32_t Blue;
+
+                    Red = round(sqrt((Gx.red * Gx.red) + (Gy.red * Gy.red)));
+                    Green = round(sqrt((Gx.green * Gx.green) + (Gy.green * Gy.green)));
+                    Blue = round(sqrt((Gx.blue * Gx.blue) + (Gy.blue * Gy.blue)));
+
+                    if (Red > 255)
+                    {
+                    Red = 255;
+                    }
+                    if (Green > 255)
+                    {
+                    Green = 255;
+                    }
+                    if (Blue > 255)
+                    {
+                    Blue = 255;
+                    }
+
+                    // Change the pixel on the image
+                    image[h][w].rgbtBlue = Blue;
+                    image[h][w].rgbtGreen = Green;
+                    image[h][w].rgbtRed = Red;
+                }
+            }
+        }
+    }
+
+    free(og_image);
     return;
 }
