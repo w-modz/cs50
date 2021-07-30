2644d68f8f7c1238b13b63fff7a03b4d187402a0
w-modz <46196436+w-modz@users.noreply.github.com>
2021-07-30 16:47:54 +0200

    Extract logic to functions

--- week04/recover/recover.c
+++ week04/recover/recover.c
@@ -8,9 +8,11 @@
 
 typedef uint8_t BYTE;
 
-int8_t *get_filename(int32_t *imageCount);
+bool is_jpg_start(BYTE *buffer);
+FILE *supersede_file(FILE *file, int32_t *fileCount);
+char *get_filename(int32_t *imageCount);
 
-int32_t main(int32_t argc, int8_t *argv[])
+int32_t main(int32_t argc, char *argv[])
 {
     if (argc != 2)
     {
@@ -18,7 +20,7 @@ int32_t main(int32_t argc, int8_t *argv[])
         return 1;
     }
 
-    int8_t *filename = argv[1];
+    char *filename = argv[1];
     FILE *inptr = fopen(filename, "r");
     if (inptr == NULL)
     {
@@ -37,24 +39,16 @@ int32_t main(int32_t argc, int8_t *argv[])
     int32_t imageCount = 1;
     while (fread(buffer, sizeof(BYTE), BLOCK_LENGTH, inptr))
     {
-        // If we find a new JPG image...
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        if (is_jpg_start(buffer))
         {
-            if (currentImg)
+            if (!(currentImg = supersede_file(currentImg, &imageCount)))
             {
-                fclose(currentImg);
-                imageCount++;
-            }
-
-            int8_t *imageName = get_filename(&imageCount);
-            currentImg = fopen(imageName, "w");
-            if (!currentImg)
-            {
-                fprintf(stderr, "Could not create a new image copy. [imageName=%s]\n", imageName);
+                fprintf(stderr, "Could not create a new image copy. [imageCount=%i]\n", imageCount);
                 free(buffer);
                 fclose(inptr);
                 return 3;
             }
+
             fwrite(buffer, sizeof(BYTE), BLOCK_LENGTH, currentImg);
         }
         else if (currentImg)
@@ -77,9 +71,26 @@ int32_t main(int32_t argc, int8_t *argv[])
     fclose(inptr);
 }
 
-int8_t *get_filename(int32_t *fileCount)
+bool is_jpg_start(BYTE *buffer)
 {
-    static int8_t fileName[8] = "000.jpg";
+    return buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+}
+
+FILE *supersede_file(FILE *file, int32_t *fileCount)
+{
+    if (file)
+    {
+        fclose(file);
+        (*fileCount)++;
+    }
+
+    char *fileName = get_filename(fileCount);
+    return fopen(fileName, "w");
+}
+
+char *get_filename(int32_t *fileCount)
+{
+    static char fileName[8] = "000.jpg";
     fileName[0] = *fileCount / 100 + 48;
     fileName[1] = *fileCount / 10 % 10 + 48;
     fileName[2] = *fileCount % 10 + 48;
