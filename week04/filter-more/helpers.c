76ab54b80a84446c0704989b5fffb03ab732bf15
amrwc <42574496+amrwc@users.noreply.github.com>
2021-07-31 10:00:36 +0200

    Refactor Week 4 Task 1 More Comfortable Solution (#3)

--- week04/filter-more/helpers.c
+++ week04/filter-more/helpers.c
@@ -1,9 +1,13 @@
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 #include "helpers.h"
+
+#define ERR_OUT_OF_MEMORY -1
+
+RGBTRIPLE** copy_image(int height, int width, RGBTRIPLE image[height][width]);
 
 const int GxKernel[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
 const int GyKernel[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
@@ -13,15 +17,13 @@ typedef struct
     float red;
     float green;
     float blue;
-}
-vertical;
+} vertical;
 typedef struct
 {
     float red;
     float green;
     float blue;
-}
-horizontal;
+} horizontal;
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -44,19 +46,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Make a copy of the original image
-    RGBTRIPLE(*og_image)[width] = calloc(height, width * sizeof(RGBTRIPLE));
-    if (og_image == NULL)
-    {
-        fprintf(stderr, "Not enough memory to store image copy.\n");
-        return;
-    }
-    memcpy(og_image, image, height * width * sizeof(RGBTRIPLE));
+    RGBTRIPLE **ogImage = copy_image(height, width, image);
 
     RGBTRIPLE adj[9];
-    for(int32_t h = 0; h < height; h++)
+    for (int32_t h = 0; h < height; h++)
     {
-        for(int32_t w = 0; w < width; w++)
+        for (int32_t w = 0; w < width; w++)
         {
             int32_t min_height = h - 1;
             int32_t max_height = h + 1;
@@ -71,7 +66,6 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             Gy.red = 0;
             Gy.green = 0;
             Gy.blue = 0;
- 
 
             // Traverse the 3x3 grid of adjacent pixels and calculate Gx and Gy
             int32_t adjIndex = 0;
@@ -91,17 +85,17 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                     }
                     else
                     {
-                        adj[adjIndex] = og_image[h2][w2];
+                        adj[adjIndex] = ogImage[h2][w2];
                     }
 
-                    Gx.red += (int32_t) adj[adjIndex].rgbtRed * GxKernel[index2][index1];
-                    Gx.green += (int32_t) adj[adjIndex].rgbtGreen * GxKernel[index2][index1];
-                    Gx.blue += (int32_t) adj[adjIndex].rgbtBlue * GxKernel[index2][index1];
-                    Gy.red += (int32_t) adj[adjIndex].rgbtRed * GyKernel[index2][index1];
-                    Gy.green += (int32_t) adj[adjIndex].rgbtGreen * GyKernel[index2][index1];
-                    Gy.blue += (int32_t) adj[adjIndex].rgbtBlue * GyKernel[index2][index1];
+                    Gx.red += (int32_t)adj[adjIndex].rgbtRed * GxKernel[index2][index1];
+                    Gx.green += (int32_t)adj[adjIndex].rgbtGreen * GxKernel[index2][index1];
+                    Gx.blue += (int32_t)adj[adjIndex].rgbtBlue * GxKernel[index2][index1];
+                    Gy.red += (int32_t)adj[adjIndex].rgbtRed * GyKernel[index2][index1];
+                    Gy.green += (int32_t)adj[adjIndex].rgbtGreen * GyKernel[index2][index1];
+                    Gy.blue += (int32_t)adj[adjIndex].rgbtBlue * GyKernel[index2][index1];
                     adjIndex++;
-                    if(index1 == 2)
+                    if (index1 == 2)
                     {
                         index1 = 0;
                         index2++;
@@ -121,15 +115,15 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 
                     if (Red > 255)
                     {
-                    Red = 255;
+                        Red = 255;
                     }
                     if (Green > 255)
                     {
-                    Green = 255;
+                        Green = 255;
                     }
                     if (Blue > 255)
                     {
-                    Blue = 255;
+                        Blue = 255;
                     }
 
                     // Change the pixel on the image
@@ -141,6 +135,29 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-    free(og_image);
+    free(ogImage);
     return;
+}
+
+RGBTRIPLE **copy_image(int height, int width, RGBTRIPLE image[height][width])
+{
+    RGBTRIPLE **copy = NULL;
+    if (!(copy = (RGBTRIPLE **)calloc(height, sizeof(RGBTRIPLE))))
+    {
+        fprintf(stderr, "Not enough memory to store image copy.\n");
+        exit(ERR_OUT_OF_MEMORY);
+    }
+
+    for (uint32_t row = 0; row < width; row++)
+    {
+        if (!(copy[row] = (RGBTRIPLE *)calloc(width, sizeof(RGBTRIPLE))))
+        {
+            fprintf(stderr, "Not enough memory to store image copy.\n");
+            free(copy);
+            exit(ERR_OUT_OF_MEMORY);
+        }
+        memcpy(copy[row], image[row], width * sizeof(RGBTRIPLE));
+    }
+
+    return copy;
 }
