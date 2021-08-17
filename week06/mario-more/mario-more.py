6d16327048982ee19b2c86592286e017623c84f3
w-modz <46196436+w-modz@users.noreply.github.com>
2021-08-17 14:33:36 +0200

    Implement 'mario-more'

---
+++ week06/mario-more/mario-more.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+
+answer = 9
+while answer < 1 or answer > 8:
+    answer = get_int('Height: ')
+index = 0
+while answer > 0:
+    print(' ' * (answer - 1), end='')
+    print('#' * (index + 1), end=' ')
+    print('#' * (index + 1))
+    index += 1
+    answer -= 1
