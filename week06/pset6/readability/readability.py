cf674a62e797df89c08ab8f3a43bd20d3175e1a5
w-modz <46196436+w-modz@users.noreply.github.com>
2021-08-19 17:14:33 +0200

    Test `readability`, fix Liau Index

--- week06/pset6/readability/readability.py
+++ week06/pset6/readability/readability.py
@@ -1,39 +1,18 @@
-from cs50 import get_string
+import liau_index
 
 
 def main():
     text: str = ''
     while len(text) < 1:
-        text = get_string('Input text: ')
-    sentenceCount = 1
-    wordCount = 1
-    letterCount = 1
+        text = input('Input text: ')
 
-    for index in range(len(text) - 1):
-        current_char = text[index]
-        if current_char.isalpha():
-            letterCount += 1
-        elif text[index].isspace():
-            wordCount += 1
-        elif is_punctuation(text[index]):
-            sentenceCount += 1
-        index += 1
-    L = letterCount / wordCount * 100
-    S = sentenceCount / wordCount * 100
-    LiauIndex = 0.0588 * L - 0.296 * S - 15.8
-    grade = round(LiauIndex, 0)
-
+    grade = liau_index.calculate_index(text)
     if grade < 1:
         print('Before Grade 1')
     elif grade <= 16:
         print('Grade ' + str(int(grade)))
     else:
         print('Grade 16+')
-
-
-def is_punctuation(char: str) -> bool:
-    symbols = {'.', '!', '?'}
-    return char in symbols
 
 
 main()
