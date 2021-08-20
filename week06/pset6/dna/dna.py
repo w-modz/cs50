6eafa42fd039ba2ea07c6154ff89083f5449b3f1
w-modz <46196436+w-modz@users.noreply.github.com>
2021-08-20 21:10:41 +0200

    Implement `dna` functionality, implement tests for `check_arguments()`, `compare()`, `main()`

--- week06/pset6/dna/dna.py
+++ week06/pset6/dna/dna.py
@@ -1,14 +1,43 @@
+import csv
 import sys
 
 
-def main() -> None:
+def main() -> str:
     if not check_arguments(sys.argv):
-        print("Usage: dna.py <STR counts path> <DNA sequence path>")
-        return
+        return "Usage: dna.py <STR counts path> <DNA sequence path>"
+    dna_database = open(sys.argv[1])
+    dna_file = open(sys.argv[2])
+    data_reader = csv.DictReader(dna_database)
+    STR_names = data_reader.fieldnames[1:]
+    dna_sequence = dna_file.read()
+    dna_file.close()
+    STR_count = {}
+    for STR in STR_names:
+        STR_count[STR] = repeats(STR, dna_sequence)
+    for row in data_reader:
+        if compare(STR_names, STR_count, row):
+            dna_database.close()
+            return f'{row["name"]}'
+    dna_database.close()
+    return "No match found"
 
 
 def check_arguments(argv: list[str]) -> bool:
     return len(argv) == 3
 
 
-main()
+def repeats(STR: str, dna: str) -> int:
+    index = 0
+    while (index + 1) * STR in dna:
+        index += 1
+    return index
+
+
+def compare(STR_names: list[str], STR_count: dict, row) -> bool:
+    for STR in STR_names:
+        if int(row[STR]) != STR_count[STR]:
+            return False
+        return True
+
+
+print(main())
--- week06/pset6/dna/dna_test.py
+++ week06/pset6/dna/dna_test.py
@@ -1,5 +1,10 @@
+import sys
+
 import pytest
-import dna
+from dna import repeats
+from dna import check_arguments
+from dna import compare
+from dna import main
 
 
 @pytest.mark.parametrize('argv, expected', [
@@ -11,5 +16,38 @@ import dna
     (['dna/dna.py', "foo"], False),
 ])
 def test_check_arguments(argv: list[str], expected: bool) -> None:
-    result = dna.check_arguments(argv)
+    result = check_arguments(argv)
+    assert result == expected
+
+
+#TODO: fix test for repeats()
+@pytest.mark.parametrize('STR, dna, expected', [
+    (['AGATC'], ['AGATCAGATCAGATC\n'], 3),
+    (['AATG'], ['AATGAATGTATC\n'], 2),
+    (['AATG'], ['AATGTATCAATGAATGTATCAATG\n'], 2),
+    (['TATC'], ['AGATCAATG\n'], 0),
+])
+def test_repeats(STR: str, dna: str, expected: int) -> None:
+    result = repeats(STR, dna)
+    assert result == expected
+
+
+@pytest.mark.parametrize('STR_names, STR_count, row, expected', [
+    (['AGATC', 'TTTTTTCT', 'AATG', 'TCTAG', 'GATA', 'TATC', 'GAAA', 'TCTG'],
+     {'AGATC': 37, 'TTTTTTCT': 40, 'AATG': 10, 'TCTAG': 6, 'GATA': 5, 'TATC': 10, 'GAAA': 28, 'TCTG': 8},
+     {'name': 'Fred', 'AGATC': '37', 'TTTTTTCT': '40', 'AATG': '10', 'TCTAG': '6', 'GATA': '5', 'TATC': '10',
+      'GAAA': '28', 'TCTG': '8'}, True),
+])
+def test_compare(STR_names: list[str], STR_count: dict, row, expected: bool) -> None:
+    result = compare(STR_names, STR_count, row)
+    assert result == expected
+
+
+
+@pytest.mark.parametrize('argv, expected', [
+    (['C:/Users/vitom/Documents/programming/cs50/week06/pset6/dna/dna.py', 'databases/large.csv', 'sequences/19.txt'], 'Fred'),
+])
+def test_main(argv: list[str], expected: str) -> None:
+    sys.argv = argv
+    result = main()
     assert result == expected
