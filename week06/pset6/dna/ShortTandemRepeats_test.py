f3473c4069f54a7e17014f2fe6da63a914f5d2ac
amrwc <42574496+amrwc@users.noreply.github.com>
2021-08-23 13:08:46 +0200

    Unit test `equals_ignoring_name()`

--- week06/pset6/dna/ShortTandemRepeats_test.py
+++ week06/pset6/dna/ShortTandemRepeats_test.py
@@ -1,6 +1,28 @@
 import pytest
 
 import ShortTandemRepeats
+from ShortTandemRepeats import ShortTandemRepeats as STR
+
+
+@pytest.mark.parametrize('str1, str2, expected', [
+    (
+        STR({'name': 'Bar', 'AGATC': 18}),
+        STR({'name': 'Foo', 'AGATC': 18}),
+        True,
+    ),
+    (
+        STR({'name': 'Foo', 'AGATC': 18}),
+        STR({'name': 'Foo', 'AGATC': 4}),
+        False,
+    ),
+    (
+        STR({'name': 'Bar', 'AGATC': 18}),
+        STR({'name': 'Foo', 'AGATC': 4}),
+        False,
+    ),
+])
+def test_equals_ignoring_name(str1: STR, str2: STR, expected: bool) -> None:
+    assert str1.equals_ignoring_name(str2) == expected
 
 
 @pytest.mark.parametrize('str_label, dna_sequence, expected', [
